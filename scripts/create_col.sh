cc2eded62fbee96479cf52b1db73f884c9366b0c
Matthias Petri <Matthias.Petri@gmail.com>
2019-01-12 16:15:15 +1100

    fix create col script

--- scripts/create_col.sh
+++ scripts/create_col.sh
@@ -3,17 +3,20 @@
 INPUT_FILE=$1
 COL_DIR=$2
 
+echo "create col dir $COL_DIR"
 mkdir -p $COL_DIR
 
+echo "copy input to dir $COL_DIR"
 cp $INPUT_FILE $COL_DIR/raw_input.json
 
+echo "random shuffle input"
 sort -R $COL_DIR/raw_input.json > $COL_DIR/shuf_input.json
-
 
 NUM_TEST=1000
 NUM_DEV=1000
 NUM_DEV_AND_TEST=2000
 
+echo "create dev/test/train files"
 tail -n $NUM_DEV_AND_TEST $COL_DIR/shuf_input.json > $COL_DIR/dev_and_test.json
 
 head -n $NUM_DEV $COL_DIR/dev_and_test.json > $COL_DIR/dev.json
@@ -22,6 +25,7 @@ tail -n $NUM_TEST $COL_DIR/dev_and_test.json > $COL_DIR/test.json
 
 head -n -$NUM_DEV_AND_TEST $COL_DIR/shuf_input.json > $COL_DIR/train.json
 
+NUM_QUERIES$(wc -l $COL_DIR/shuf_input.json)
 NUM_TRAIN=$(wc -l $COL_DIR/train.json)
 NUM_DEV=$(wc -l $COL_DIR/dev.json)
 NUM_TEST=$(wc -l $COL_DIR/test.json)
