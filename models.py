8a1ebb22b48b1adda15bed895f7b1b6b92d16904
Matthias Petri <Matthias.Petri@gmail.com>
2019-01-20 15:39:59 +1100

    dropout

--- models.py
+++ models.py
@@ -98,7 +98,7 @@ import torch.nn.functional as F
 
 
 class MLP(nn.Module):
-    def __init__(self,num_layers=hyperparams.default_num_layers):
+    def __init__(self, num_layers=hyperparams.default_num_layers):
         super(MLP, self).__init__()
         self.num_layers = num_layers
         self.input_dim = hyperparams.default_max_qry_len * hyperparams.num_term_params
@@ -107,10 +107,10 @@ class MLP(nn.Module):
             self.layers.add_module("linear_{}".format(
                 i + 1), nn.Linear(self.input_dim, self.input_dim))
             self.layers.add_module("ReLU_{}".format(i + 1), nn.ReLU())
+            self.layers.add_module("Dropout_{}".format(
+                i + 1), nn.Dropout(p=hyperparams.default_dropout))
         self.layers.add_module("last_linear", nn.Linear(self.input_dim, 1))
 
     def forward(self, queries):
         pred_thres = self.layers(queries)
         return pred_thres
-
-
