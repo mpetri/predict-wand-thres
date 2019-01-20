0f48a3ff4318514fa2bf1f1cd94c9db338dd1eb0
Matthias Petri <Matthias.Petri@gmail.com>
2019-01-20 15:32:19 +1100

    add k to train params

--- scripts/train_mlp.sh
+++ scripts/train_mlp.sh
@@ -4,9 +4,11 @@ COL=$1
 DEVICE=$2
 LAYERS=$3
 QUANT=$4
+TOPK=$5
 
 python ../train.py --data_dir $COL \
                    --device $DEVICE \
                    --layers $LAYERS \
                    --batch_size 256 \
+                   --k $TOPK \
                    --quantile $QUANT
