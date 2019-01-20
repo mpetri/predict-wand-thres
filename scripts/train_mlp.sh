84be00965d4fcd9ba1e9822a84cab993bdaa9229
Matthias Petri <Matthias.Petri@gmail.com>
2019-01-20 15:34:03 +1100

    another training script

--- scripts/train_mlp.sh
+++ scripts/train_mlp.sh
@@ -3,12 +3,28 @@
 COL=$1
 DEVICE=$2
 LAYERS=$3
-QUANT=$4
-TOPK=$5
 
-python ../train.py --data_dir $COL \
-                   --device $DEVICE \
-                   --layers $LAYERS \
-                   --batch_size 256 \
-                   --k $TOPK \
-                   --quantile $QUANT
+for QUANT in "0.5 0.7 0.9 0.99"
+do
+    python ../train.py --data_dir $COL \
+                    --device $DEVICE \
+                    --layers $LAYERS \
+                    --batch_size 256 \
+                    --k 10 \
+                    --quantile $QUANT
+
+    python ../train.py --data_dir $COL \
+                    --device $DEVICE \
+                    --layers $LAYERS \
+                    --batch_size 256 \
+                    --k 100 \
+                    --quantile $QUANT
+
+
+    python ../train.py --data_dir $COL \
+                    --device $DEVICE \
+                    --layers $LAYERS \
+                    --batch_size 256 \
+                    --k 1000 \
+                    --quantile $QUANT
+done
